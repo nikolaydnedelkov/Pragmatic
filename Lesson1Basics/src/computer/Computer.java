24635e22a64565dcd67df6ee0c1e3280105bf5f6
Nikolay Nedelkov <nikolayd.nedelkov@gmail.com>
2020-01-21 21:09:02 +0200

    Computer task homework

---
+++ Lesson1Basics/src/computer/Computer.java
@@ -0,0 +1,43 @@
+package computer;
+
+public class Computer {
+    private int year;
+    private double price;
+    private boolean isNotebook;
+    private int hardDiskMemory;
+    private int freeMemory;
+    private String operationSystem;
+
+    public Computer(int year, double price, boolean isNotebook, int hardDiskMemory, int freeMemory, String operationSystem) {
+        this.year = year;
+        this.price = price;
+        this.isNotebook = isNotebook;
+        this.hardDiskMemory = hardDiskMemory;
+        this.freeMemory = freeMemory;
+        this.operationSystem = operationSystem;
+    }
+
+    @Override
+    public String toString() {
+        return "Computer{" +
+                "year=" + year +
+                ", price=" + price +
+                ", isNotebook=" + isNotebook +
+                ", hardDiskMemory=" + hardDiskMemory +
+                ", freeMemory=" + freeMemory +
+                ", operationSystem='" + operationSystem + '\'' +
+                '}';
+    }
+
+    public void changeOperationSystem(String newOperationSystem){
+        this.operationSystem = newOperationSystem;
+    }
+
+    public void useMemory(double memory){
+        if(memory<this.freeMemory) {
+            this.freeMemory -= memory;
+        }else{
+            System.out.println("Not enough free memory!");
+        }
+    }
+}
---
+++ Lesson1Basics/src/computer/ComputerDemo.java
@@ -0,0 +1,22 @@
+package computer;
+
+public class ComputerDemo {
+    public static void main(String[] args) {
+        Computer pc = new Computer(2012, 500, false, 1000,300,"Windows 10");
+        Computer mac = new Computer(2019,4500, true, 2048, 1560, "macOS");
+
+        System.out.println(pc.toString());
+        System.out.println(mac.toString());
+
+        System.out.println();
+
+        pc.changeOperationSystem("Ubunto");
+        mac.useMemory(1700);
+        mac.useMemory(300);
+
+        System.out.println();
+
+        System.out.println(pc.toString());
+        System.out.println(mac.toString());
+    }
+}
